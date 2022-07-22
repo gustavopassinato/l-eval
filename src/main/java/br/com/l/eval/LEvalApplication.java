3d54064dcf81754118b7578ba957191139150123
Gustavo Passinato Neves <passinato11@gmail.com>
2022-07-22 01:00:37 -0300

    Primeiro commit

---
+++ src/main/java/br/com/l/eval/LEvalApplication.java
@@ -0,0 +1,13 @@
+package br.com.l.eval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LEvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LEvalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/l/eval/LEvalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.l.eval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LEvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
